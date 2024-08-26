4bdcb857a4db87f6bf6e48a05af054cf66e990f9
Dave Banwell <dave@davebanwell.com>
2024-08-25 20:49:57 -0400

    passes all tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+	const [dogImg, setDogImg] = useState(null);
+	const altText = "A Random Dog";
+
+	useEffect(() => {
+		fetch('https://dog.ceo/api/breeds/image/random')
+			.then(res => res.json())
+            .then(dog => setDogImg(dog.message));
+	}, []);
+
+	switch (dogImg === null) {
+		case true:
+			return <p>Loading...</p>
+		case false:
+        default:
+			return <img src={dogImg} alt={altText} />
+	};
+};
+
+export default App;
